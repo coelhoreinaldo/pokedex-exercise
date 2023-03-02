cf8e8acdfda7ba8342d8e3d179729d3a2d8ab506
coelhoreinaldo <scoelhor7@gmail.com>
2023-03-02 19:01:31 -0300

    feat: Requisito 2

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -5,15 +5,32 @@ import Pokemon from './Pokemon';
 import { pokemonType } from '../types';
 
 class Pokedex extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      pokemonIndex: 0,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState((previousState) => ({
+      pokemonIndex: previousState.pokemonIndex + 1,
+    }));
+  }
+
   render() {
+    const { pokemonIndex } = this.state;
     const { pokemonList } = this.props;
     return (
       <>
         <h1> Pokédex </h1>
         <div className="pokedex">
-          <Pokemon pokemon={ pokemonList[0] } />
+          <Pokemon pokemon={ pokemonList[pokemonIndex] } />
+          <button onClick={ this.handleClick }>Próximo pokemon</button>
           {/* { pokemonList
-            .map((pokemon) => <Pokemon key={ pokemon.id } pokemon={ pokemon } />) } */
+            .map((pokemon) => <Pokemon key={ pok  emon.id } pokemon={ pokemon } />) } */
           }
         </div>
       </>
